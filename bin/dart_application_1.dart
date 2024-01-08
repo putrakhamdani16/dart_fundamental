23b03d6d5481cda923c3c61e9dd8167f6dc8beac
putrakhamdani <putrakhamdani16@gmail.com>
2024-01-08 19:39:09 +0700

    update

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,18 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
+/// Fungsi [main] akan menampilkan 2 output
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+  // Mencetak Hello Dart! Dart is great. pada konsol
+  print('Hello Dart! Dart is great.');
+  // Testing documentation comment with [].
+  print('6 * 7 = ${calculate()}');
 }
+
+int calculate() {
+  return 6 * 7;
+}
+
+/// Output:
+/// Hello Dart! Dart is great.
+/// 6 * 7 = 42
